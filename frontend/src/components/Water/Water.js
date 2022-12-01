22dc7716083bf0b922245759b51ce18ffcfd621f
kelciefan <kelciefa@usc.edu>
2022-12-01 14:31:22 -0800

    water button update

--- frontend/src/components/Water/Water.js
+++ frontend/src/components/Water/Water.js
@@ -1,19 +1,42 @@
 import React from 'react';
 import './Water.css'
-import {useState} from 'react';
+import {useState, useLocation} from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Water({handleClick, notClicked}) {
     const water = require('../../assets/water.png');
     const watered = require('../../assets/waterPress.png');
     const images = {water,watered};
     const [img, setImg] = useState(false);
+    const [canWater, setWater] = useState(false);
+    const [home, setHome] = useState(false);
 
+    // const {state} = useLocation();
+    // var id = state.id;
 
-    const imgChangeHandler = () => {
-        if(!img) {
+    const navigate = useNavigate();
+
+    const getResponse = async() => {
+        const response = await fetch("", {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json'},
+        })
+
+        const data = response.json();
+        console.log(data);
+
+        const obj = JSON.parse(data);
+
+        if(obj.error == false) {
+            setWater(true);
+        }
+    }
+
+    const imgChangeHandler = (e) => {
+        if(canWater) {
             setImg(true);
-        }else{
-            setImg(false)
+            setWater(false);
+            navigate("/home");
         }
     };
 
